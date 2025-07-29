0ea57abfa8371a14937805c2bef6942577477eb0
Abdelrahman Salah <abdelrahmansalah211@gmail.com>
2025-07-29 16:35:21 +0300

    chore: add ClockfiyTimeEntryId

--- task/Models/TimeEntry.cs
+++ task/Models/TimeEntry.cs
@@ -7,11 +7,12 @@ namespace task.Models
     public int Id { get; set; }
     public DateTime Start { get; set; }
     public DateTime End { get; set; }
+    public string? ClockifyTimeEntryId { get; set; }
     public int UserId { get; set; }
     public int TaskId { get; set; }
     public int ProjectId { get; set; }
-    public required User User { get; set; }
-    public required Task Task { get; set; }
-    public required Project Project { get; set; }
+    public User? User { get; set; }
+    public Task? Task { get; set; }
+    public Project? Project { get; set; }
   }
 }
