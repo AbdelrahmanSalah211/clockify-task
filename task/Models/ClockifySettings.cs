9995e4caaef667739c027fdcc8738f1161d91da0
Abdelrahman Salah <abdelrahmansalah211@gmail.com>
2025-07-29 15:20:12 +0300

    chore: clockify settings

---
+++ task/Models/ClockifySettings.cs
@@ -0,0 +1,8 @@
+namespace task.Models
+{
+  public class ClockifySettings
+  {
+    public string ApiKey { get; set; } = string.Empty;
+    public string WorkspaceId { get; set; } = string.Empty;
+  }
+}
