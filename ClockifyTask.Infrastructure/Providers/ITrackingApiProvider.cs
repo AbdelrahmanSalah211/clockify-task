caef382b70ca571bd030259bdd62df1146fccc5c
Abdelrahman Salah <abdelrahmansalah211@gmail.com>
2025-08-09 22:01:20 +0300

    move file from infrastructure to application so that application not depende on infra

--- ClockifyTask.Infrastructure/Providers/ITrackingApiProvider.cs
+++
@@ -1,6 +0,0 @@
-public interface ITrackingApiProvider
-{
-    Task<string> CreateTrackingProjectAsync(ProjectTrackingDto projectDto);
-    Task<string> CreateTrackingTaskAsync(TaskTrackingDto taskDto);
-    Task<string> CreateTrackingTimeEntryAsync(TimeEntryTrackingDto timeEntryDto);
-}
