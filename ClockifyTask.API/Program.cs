151105ddd6c6767d1db2924be3cd7e655afa23b4
Abdelrahman Salah <abdelrahman.salah@enozom.com>
2025-08-11 12:49:12 +0300

    chore: move dependecies registeration to program.cs

--- ClockifyTask.API/Program.cs
+++ ClockifyTask.API/Program.cs
@@ -1,18 +1,53 @@
-using ClockifyTask.Application;
-using ClockifyTask.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+// Application Layer
+using ClockifyTask.Application.Interfaces;
+using ClockifyTask.Application.Services;
+
+// Domain Layer  
+using ClockifyTask.Domain.Interfaces;
+using ClockifyTask.Domain.Entities;
+
+// Infrastructure Layer
+using ClockifyTask.Infrastructure.Persistence;
+using ClockifyTask.Infrastructure.Providers;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// Database Context
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("DefaultConnection"))));
+
+// Unit of Work
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+// Repositories
+builder.Services.AddScoped<IProjectRepository, ProjectRepository>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<ITimeEntryRepository, TimeEntryRepository>();
+builder.Services.AddScoped<IAssignedTaskRepository, AssignedTaskRepository>();
+
+// Application Services
+builder.Services.AddScoped<IProjectService, ProjectService>();
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<ITimeEntryService, TimeEntryService>();
+builder.Services.AddScoped<IAssignedTaskService, AssignedTaskService>();
+builder.Services.AddScoped<IReportService, ReportService>();
+
+// External API Provider
+builder.Services.AddScoped<ITrackingApiProvider, ClockifyApiProvider>();
+builder.Services.AddHttpClient();
+
+// Configuration
+var clockifySettings = builder.Configuration.GetSection("Clockify").Get<ClockifySettings>();
+builder.Services.AddSingleton(clockifySettings ?? new ClockifySettings());
+
+// API Services
 builder.Services.AddControllers();
-
-// Register Application Layer services
-builder.Services.AddApplication(builder.Configuration);
-
-// Register Infrastructure Layer services (includes DbContext)
-builder.Services.AddInfrastructure(builder.Configuration);
-
-// Add Swagger/OpenAPI
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
