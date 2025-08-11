c12c33d6bf39b22f535e480d59bb609466b12c2c
Abdelrahman Salah <abdelrahman.salah@enozom.com>
2025-08-11 12:19:14 +0300

    rm: remove dependecies on repositories that violated o/c principle as db context is shared because of being registered as scopped

--- ClockifyTask.Domain/Interfaces/IUnitOfWork.cs
+++ ClockifyTask.Domain/Interfaces/IUnitOfWork.cs
@@ -1,12 +1,7 @@
 namespace ClockifyTask.Domain.Interfaces
 {
     public interface IUnitOfWork : IDisposable
-    {
-        IProjectRepository Projects { get; }
-        IUserRepository Users { get; }
-        IAssignedTaskRepository AssignedTasks { get; }
-        ITimeEntryRepository TimeEntries { get; }
-        
+    {   
         Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
     }
 }
--- ClockifyTask.Infrastructure/Persistence/UnitOfWork.cs
+++ ClockifyTask.Infrastructure/Persistence/UnitOfWork.cs
@@ -2,26 +2,12 @@ using ClockifyTask.Domain.Interfaces;
 
 namespace ClockifyTask.Infrastructure.Persistence
 {
-
     public class UnitOfWork : IUnitOfWork
     {
-        private readonly IProjectRepository _projectRepository;
-        private readonly ITimeEntryRepository _timeEntryRepository;
-        private readonly IUserRepository _userRepository;
-        private readonly IAssignedTaskRepository _assignedTaskRepository;
         private readonly ApplicationDbContext _context;
         private bool _disposed;
-        public UnitOfWork(
-            IProjectRepository projectRepository,
-            ITimeEntryRepository timeEntryRepository,
-            IUserRepository userRepository,
-            IAssignedTaskRepository assignedTaskRepository,
-            ApplicationDbContext context)
+        public UnitOfWork(ApplicationDbContext context)
         {
-            _projectRepository = projectRepository;
-            _timeEntryRepository = timeEntryRepository;
-            _userRepository = userRepository;
-            _assignedTaskRepository = assignedTaskRepository;
             _context = context;
         }
 
@@ -29,10 +15,6 @@ namespace ClockifyTask.Infrastructure.Persistence
         {
             return await _context.SaveChangesAsync(cancellationToken);
         }
-        public IProjectRepository Projects => _projectRepository;
-        public IUserRepository Users => _userRepository;
-        public IAssignedTaskRepository AssignedTasks => _assignedTaskRepository;
-        public ITimeEntryRepository TimeEntries => _timeEntryRepository;
 
         public void Dispose()
         {
