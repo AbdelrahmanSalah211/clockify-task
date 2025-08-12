410d130d63d1cc16f59d71aa080c7b502705989a
Abdelrahman Salah <abdelrahman.salah@enozom.com>
2025-08-12 17:34:32 +0300

    feat: register and login user

---
+++ ClockifyTask.API/Controllers/authController.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Mvc;
+using ClockifyTask.Application.DTOs;
+using ClockifyTask.Application.Interfaces;
+
+namespace ClockifyTask.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterDto registerDto)
+        {
+            var result = await _authService.RegisterAsync(registerDto);
+            if (!result)
+            {
+                return BadRequest("Registration failed");
+            }
+
+            return Ok(new
+            {
+                Message = "Registration successful"
+            });
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginDto loginDto)
+        {
+            var result = await _authService.LoginAsync(loginDto);
+            if (result == null)
+            {
+                return Unauthorized();
+            }
+            return Ok(result);
+        }
+    }
+}
