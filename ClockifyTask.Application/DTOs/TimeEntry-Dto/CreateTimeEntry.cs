f50271b20dd5d23ea91724b912719d4bb10752ef
Abdelrahman Salah <abdelrahman.salah@enozom.com>
2025-08-12 12:13:28 +0300

    chore: relationship with time entry to depend on task only

--- ClockifyTask.Application/DTOs/TimeEntry-Dto/CreateTimeEntry.cs
+++ ClockifyTask.Application/DTOs/TimeEntry-Dto/CreateTimeEntry.cs
@@ -2,6 +2,6 @@ public record CreateTimeEntryDto
 {
     public DateTime Start { get; init; }
     public DateTime End { get; init; }
-    public int UserId { get; init; }
-    public int ProjectId { get; init; }
+    public int? UserId { get; init; }
+    public int? ProjectId { get; init; }
 }
